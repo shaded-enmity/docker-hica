19f6c3d2070d1f75f1209ffbc65412e789d2502e
shaded-enmity <the.arul@gmail.com>
2016-01-14 19:37:53 +0100

    Place all injectors in /var/lib/docker-hica/injectors

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
 from setuptools import setup, find_packages
+import os, glob
+
+hica_lib = '/var/lib/docker-hica'
+injectors = os.path.join(hica_lib, 'injectors')
+
+if not os.path.exists(hica_lib):
+    os.mkdir(hica_lib)
+
+if not os.path.exists(injectors):
+    os.mkdir(injectors)
 setup(
     name = 'docker-hica',
     version = '0.5',
-    packages = find_packages() + ['injectors/'],
+    packages = find_packages(),
     scripts = ['docker-hica'],
     install_requires = ['docker-py'],
     package_data = {
@@ -14,5 +24,6 @@ setup(
     description = 'The goal of this project is to define a set of image label metadata and launcher tooling that understands said metadata to provide for smooth experience running containerized applications with tight integration with the host operating system.',
     license = 'MIT',
     keywords = 'docker host integrated container application',
-    url = 'https://github.com/shaded-enmity/docker-hica'
+    url = 'https://github.com/shaded-enmity/docker-hica',
+    data_files=[(injectors, glob.glob('injectors/*'))]
 )
